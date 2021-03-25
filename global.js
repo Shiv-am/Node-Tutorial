f2d9dabd3a6936ef399b82274cd21a7086756b1e
Shivam Bansal <shivam2032.cse18@chitkara.edu.in>
2021-03-25 08:58:00 +0530

    various global objects

---
+++ global.js
@@ -0,0 +1,9 @@
+//console.log(global);
+//settimeout and global.settimeout is one n the same thing
+
+//setTimeout(() => {
+//    console.log("This is timeout");
+//}, 4000);
+
+console.log(__dirname);
+console.log(__filename);
