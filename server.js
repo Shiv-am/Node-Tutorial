8a73b29c1021a55a48f158aeaf7d1a06126c25b2
Shivam Bansal <shivam2032.cse18@chitkara.edu.in>
2021-03-29 12:34:29 +0530

    Set header content type

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ const http = require('http');
 
 const server = http.createServer((req,res) =>{
     console.log(req.url,req.method);
+ 
+//set header contnent type
+   res.setHeader('Content-Type','text/plain');
+   res.write('Hello Shivam');
+   res.end();
+
+
+
+
 })
+
 
 server.listen(3000,'localhost',()=>{
     console.log("listening to server on 3000")
