249e4cbb9bf18567f21672aa323359924cdd3c62
Shivam Bansal <shivam2032.cse18@chitkara.edu.in>
2021-03-31 13:03:21 +0530

    Sending Html File

--- app.js
+++ app.js
@@ -8,5 +8,12 @@ const app = express();
 app.listen(3000);
 
 app.get('/',(req,res)=>{
-    res.send('<p>Hii Shivam</p>');
+    //res.send('<p>Hii Shivam</p>');
+    res.sendFile('./views/index.html',{root: __dirname});
+})
+
+app.get('/about',(req,res)=>{
+    //res.send('<p>Hii This is about page</p>');
+
+    res.sendFile('./views/about.html',{root: __dirname});
 })
