e22a66ed56019bcca4fd68a40d90049c71a55734
Shivam Bansal <shivam2032.cse18@chitkara.edu.in>
2021-04-03 11:24:43 +0530

    final code for app.js

--- app.js
+++ app.js
@@ -1,45 +1,56 @@
 const express = require('express');
+const morgan = require('morgan');
 
-//Registring view Engine
-
-app.set('view engine','ejs');
-
-//Creating Middleware
-app.use((req,res,next)=>{
-    console.log('new request made');
-    console.log('host',res.hostname());
-    console.log('path',res.path());
-    console.log('method',req.method());
-})
-
-
-
-
-//creating react app
-
+// express app
 const app = express();
 
-//Listening to server
+// listen for requests
 app.listen(3000);
 
-app.get('/',(req,res)=>{
-    //res.send('<p>Hii Shivam</p>');
-    res.sendFile('./views/index.html',{root: __dirname});
-})
+// register view engine
+app.set('view engine', 'ejs');
 
-app.get('/about',(req,res)=>{
-    //res.send('<p>Hii This is about page</p>');
+// middleware & static files
+app.use(express.static('public'));
 
-    res.sendFile('./views/about.html',{root: __dirname});
-})
+app.use((req, res, next) => {
+  console.log('new request made:');
+  console.log('host: ', req.hostname);
+  console.log('path: ', req.path);
+  console.log('method: ', req.method);
+  next();
+});
 
-//Redirects
-app.get('/about-us',(req,res)=>{
-    res.redirect('./about');
-})
+app.use((req, res, next) => {
+  console.log('in the next middleware');
+  next();
+});
 
-//404 Page
+app.use(morgan('dev'));
 
-app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html',{root: __dirname});
-})
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
+app.get('/', (req, res) => {
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
